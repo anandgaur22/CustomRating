1f3aa63dc88d76c83a2cc51b7febc02491fefa1d
iws-034 <kundan@i-webservices.com>
2018-09-21 13:45:04 +0530

    Initial commit

---
+++ app/src/main/java/com/anand/customrater/AppRater.java
@@ -0,0 +1,137 @@
+package com.anand.customrater;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.shashank.sony.fancygifdialoglib.FancyGifDialog;
+import com.shashank.sony.fancygifdialoglib.FancyGifDialogListener;
+
+public class AppRater {
+    private final static String APP_TITLE = "App Rater";
+    private final static String APP_PNAME = "com.anand.customrater";
+
+
+
+    private final static int DAYS_UNTIL_PROMPT = 0;
+    private final static int LAUNCHES_UNTIL_PROMPT = 0;
+
+    public static void app_launched(Context mContext) {
+        SharedPreferences prefs = mContext.getSharedPreferences("apprater", 0);
+        if (prefs.getBoolean("dontshowagain", false)) { return ; }
+
+        SharedPreferences.Editor editor = prefs.edit();
+
+        // Increment launch counter
+        long launch_count = prefs.getLong("launch_count", 0) + 1;
+        editor.putLong("launch_count", launch_count);
+
+        // Get date of first launch
+        Long date_firstLaunch = prefs.getLong("date_firstlaunch", 0);
+        if (date_firstLaunch == 0) {
+            date_firstLaunch = System.currentTimeMillis();
+            editor.putLong("date_firstlaunch", date_firstLaunch);
+        }
+
+        // Wait at least n days before opening
+        if (launch_count >= LAUNCHES_UNTIL_PROMPT) {
+            if (System.currentTimeMillis() >= date_firstLaunch +
+                    (DAYS_UNTIL_PROMPT * 24 * 60 * 60 * 1000)) {
+                showRateDialog(mContext, editor);
+            }
+        }
+
+        editor.commit();
+    }
+
+    public static void showRateDialog(final Context mContext, final SharedPreferences.Editor editor) {
+
+
+        new FancyGifDialog.Builder((Activity) mContext)
+                .setTitle("Rate " + APP_TITLE)
+                .setMessage("If you enjoy using " + APP_TITLE + ", please take a moment to rate it. Thanks for your support!")
+                .setNegativeBtnText("No, thanks")
+                .setPositiveBtnBackground("#FF4081")
+                .setPositiveBtnText("Rate")
+                .setNegativeBtnBackground("#FFA9A7A8")
+                .setGifResource(R.drawable.rate)   //Pass your Gif here
+                .isCancellable(true)
+                .OnPositiveClicked(new FancyGifDialogListener() {
+                    @Override
+                    public void OnClick() {
+                        mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.iws.chalenj")));
+
+                    }
+                })
+                .OnNegativeClicked(new FancyGifDialogListener() {
+                    @Override
+                    public void OnClick() {
+                        if (editor != null) {
+                            editor.putBoolean("dontshowagain", true);
+                            editor.commit();
+                        }
+
+                    }
+                })
+                .build();
+
+       /* final Dialog dialog = new Dialog(mContext);
+        dialog.setTitle("Rate " + APP_TITLE);
+
+        LinearLayout ll = new LinearLayout(mContext);
+        ll.setOrientation(LinearLayout.VERTICAL);
+
+        TextView tv = new TextView(mContext);
+        tv.setText("If you enjoy using " + APP_TITLE + ", please take a moment to rate it. Thanks for your support!");
+        tv.setWidth(240);
+        tv.setPadding(4, 0, 4, 10);
+        ll.addView(tv);
+
+        Button b1 = new Button(mContext);
+        b1.setText("Rate " + APP_TITLE);
+        b1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                mContext.startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + APP_PNAME)));
+                dialog.dismiss();
+            }
+        });
+        ll.addView(b1);
+
+        Button b2 = new Button(mContext);
+        b2.setText("Remind me later");
+        b2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dialog.dismiss();
+            }
+        });
+        ll.addView(b2);
+
+        Button b3 = new Button(mContext);
+        b3.setText("No, thanks");
+        b3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (editor != null) {
+                    editor.putBoolean("dontshowagain", true);
+                    editor.commit();
+                }
+                dialog.dismiss();
+            }
+        });
+        ll.addView(b3);
+
+        dialog.setContentView(ll);
+        dialog.show();*/
+    }
+}
